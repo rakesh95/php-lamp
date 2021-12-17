fc7519c4992f338d1891b504162585ddacd579d4
Rakesh <rakesh.birra@gmail.com>
2021-12-17 10:57:22 +0530

    up

--- php/db.php
+++ php/db.php
@@ -1,7 +1,7 @@
 <?php
 
 function Createdb(){
-    $servername = "rds-bs.cb4cljkktc4p.ap-south-1.rds.amazonaws.com";
+    $servername = "db-rds.cb4cljkktc4p.ap-south-1.rds.amazonaws.com";
     $username = "admin";
     $password = "admin123";
     $dbname = "bookstore";
